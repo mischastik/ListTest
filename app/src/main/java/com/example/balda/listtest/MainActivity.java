bae34d1d4887ca61b8d697b15142ef4df306420e
mischastik <michael.balda@gmail.com>
2017-02-17 00:40:44 +0100

    Added Firebase connectivity

--- app/src/main/java/com/example/balda/listtest/MainActivity.java
+++ app/src/main/java/com/example/balda/listtest/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.balda.listtest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends Activity implements ListAdapter.ListItemClickListener{
     private ListAdapter mAdapter;
     private RecyclerView mList;
-
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "LISTTEST";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +29,40 @@ public class MainActivity extends Activity implements ListAdapter.ListItemClickL
 
         mAdapter = new ListAdapter(10, this);
         mList.setAdapter(mAdapter);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
     }
 
     @Override
     public void onListItemClick(int clickedItemIndex) {
         // TODO: Start new activity.
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
