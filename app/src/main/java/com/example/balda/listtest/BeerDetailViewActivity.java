8b28317953c26c1c04fa21e727baf36716010172
mischastik <michael.balda@gmail.com>
2017-02-22 21:27:34 +0100

    Added inital basic beer detail activity.

---
+++ app/src/main/java/com/example/balda/listtest/BeerDetailViewActivity.java
@@ -0,0 +1,13 @@
+package com.example.balda.listtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeerDetailViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beer_detail_view);
+    }
+}
